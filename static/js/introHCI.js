1c2773089ef044b3cc482c38b10e43a831f4c11d
Nasar Issa <nkissa@ucsd.edu>
2019-01-17 10:05:34 -0800

    added Javascript!

---
+++ static/js/introHCI.js
@@ -0,0 +1,8 @@
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+}
+$("a.thumbnail").click(projectClick);
